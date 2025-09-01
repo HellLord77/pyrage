501a13cdb03a3309006d089495bcf91244b7a3d2
HellLord <ratul.debnath.year@gmail.com>
2025-09-01 08:38:18 +0600

    Add KaraokeMugen and Romm storage backends
    Introduces KaraokeMugenStorage and RommStorage classes to support file operations for Karaoke Mugen and Romm platforms. These classes implement file listing and retrieval, with partial support for file upload and deletion.

---
+++ pyrage/storage/karaokemugen.py
@@ -0,0 +1,51 @@
+from typing import Iterable
+from typing import Optional
+
+from requests import Session
+
+from . import Storage
+from ..utils import File
+from ..utils import Readable
+from ..utils import ReadableResponse
+
+
+class KaraokeMugenStorage(Storage):
+    def __init__(
+        self, collections: Optional[Iterable[str]] = None, repository: str = "kara.moe"
+    ):
+        self._repository = f"https://{repository}"
+        self._session = Session()
+        if collections is None:
+            response = self._session.get(f"{self._repository}/api/karas/repository")
+            response.raise_for_status()
+            collections = (
+                collection
+                for collection, enable in response.json()["Manifest"][
+                    "defaultCollections"
+                ].items()
+                if enable
+            )
+        self._collections = list(collections)
+        super().__init__()
+
+    def _generate_file_list(self) -> Iterable[File]:
+        response = self._session.post(
+            f"{self._repository}/api/karas/medias",
+            json={"collections": self._collections},
+        )
+        response.raise_for_status()
+        for kara in response.json():
+            yield File(kara["mediafile"], size=kara["mediasize"])
+
+    def _get_file(self, file: File) -> Readable:
+        return ReadableResponse(
+            self._session.get(
+                f"{self._repository}/downloads/medias/{file.path}", stream=True
+            )
+        )
+
+    def _set_file(self, file: File, readable: Readable):
+        raise NotImplementedError
+
+    def _del_file(self, file: File):
+        raise NotImplementedError
---
+++ pyrage/storage/romm.py
@@ -0,0 +1,77 @@
+from datetime import datetime
+from typing import Iterable
+from typing import Optional
+
+from requests import Session
+
+from . import Storage
+from ..utils import File
+from ..utils import Readable
+from ..utils import ReadableResponse
+
+
+class RommStorage(Storage):
+    def __init__(
+        self,
+        platform_id: int | str,
+        host: str = "https://demo.romm.app",
+        auth: Optional[tuple[str, str]] = None,
+    ):
+        self._host = host
+        self._session = Session()
+        self._session.auth = auth
+        if isinstance(platform_id, str):
+            response = self._session.get(f"{self._host}/api/platforms")
+            response.raise_for_status()
+            for plat in response.json():
+                if plat["slug"] == platform_id:
+                    platform_id = plat["id"]
+                    break
+            else:
+                raise NotImplementedError
+        self._platform_id = platform_id
+        super().__init__()
+
+    def _generate_file_list(self) -> Iterable[File]:
+        offset = 0
+        while True:
+            response = self._session.get(
+                f"{self._host}/api/roms",
+                params={"platform_id": self._platform_id, "offset": offset},
+            )
+            response.raise_for_status()
+            roms = response.json()
+            for rom in roms["items"]:
+                for file in rom["files"]:
+                    yield File(
+                        f'{file["rom_id"]}/{file["file_name"]}',
+                        mtime=datetime.fromisoformat(file["last_modified"]).timestamp(),
+                        size=file["file_size_bytes"],
+                        crc32=file["crc_hash"],
+                        md5=file["md5_hash"],
+                        sha1=file["sha1_hash"],
+                    )
+            offset += len(roms["items"])
+            if offset >= roms["total"]:
+                break
+
+    def _get_file(self, file: File) -> Readable:
+        rom_id, file_name = file.path.split("/", 1)
+        return ReadableResponse(
+            self._session.get(
+                f"{self._host}/api/roms/{rom_id}/content/{file_name}", stream=True
+            )
+        )
+
+    def _set_file(self, file: File, readable: Readable):
+        self._session.post(
+            f"{self._host}/api/roms",
+            readable,
+            headers={
+                "X-Upload-Platform": self._platform_id,
+                "X-Upload-Filename": file.path,
+            },
+        )
+
+    def _del_file(self, file: File):
+        raise NotImplementedError
